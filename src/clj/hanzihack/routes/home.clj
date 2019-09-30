99c34ef435379601c4bcea152b55b6541a710d11
Supakorn Warodom <supakorn@satang.com>
2019-10-01 00:17:51 +0700

    Init theme

--- src/clj/hanzihack/routes/home.clj
+++ src/clj/hanzihack/routes/home.clj
@@ -12,12 +12,17 @@
   (layout/render request "app.html"))
 
 
+(defn base-page [request]
+  (layout/render request "base.html"))
+
+
 (defn home-routes []
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/" {:get home-page}]
    ["/app" {:get app-page}]
+   ["/base" {:get base-page}]
    ["/graphiql" {:get (fn [request] (layout/render request "graphiql.html"))}]
    ["/docs" {:get (fn [_]
                     (-> (response/ok (-> "docs/docs.md" io/resource slurp))
